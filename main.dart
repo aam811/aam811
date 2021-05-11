b90ca8434dcefab52913e13f2c8661432a4995a1
aam811 <82305459+aam811@users.noreply.github.com>
2021-05-12 04:42:51 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+  home: (
+        Scaffold(body: Text("Aamir Ali Khan, ali300329@gmail.com"))
+    ),
+    );
+  }
+}
